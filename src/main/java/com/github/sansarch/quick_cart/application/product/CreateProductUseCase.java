f235eb2a69ffae7a957808514e8a9aa083f90069
Santiago Negreira <sansfleitas@gmail.com>
2025-04-21 20:33:16 -0300

    Config Spring Security

--- src/main/java/com/github/sansarch/quick_cart/application/product/CreateProductUseCase.java
+++ src/main/java/com/github/sansarch/quick_cart/application/product/CreateProductUseCase.java
@@ -2,9 +2,11 @@ package com.github.sansarch.quick_cart.application.product;
 
 import com.github.sansarch.quick_cart.domain.product.Product;
 import com.github.sansarch.quick_cart.domain.product.ProductRepository;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+@Component
 public class CreateProductUseCase {
 
     private final ProductRepository productRepository;
--- src/main/java/com/github/sansarch/quick_cart/application/product/GetAllProductsUseCase.java
+++ src/main/java/com/github/sansarch/quick_cart/application/product/GetAllProductsUseCase.java
@@ -2,9 +2,11 @@ package com.github.sansarch.quick_cart.application.product;
 
 import com.github.sansarch.quick_cart.domain.product.Product;
 import com.github.sansarch.quick_cart.domain.product.ProductRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class GetAllProductsUseCase {
 
     private final ProductRepository productRepository;
