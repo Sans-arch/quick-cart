0303e7dc6d75e3095f88fb2aa5922d73126d628c
Santiago Negreira <sansfleitas@gmail.com>
2025-04-21 16:35:40 -0300

    Create docker-compose file, create db and redis services, add redis to pom, enable caching

--- src/main/java/com/github/sansarch/quick_cart/QuickCartApplication.java
+++ src/main/java/com/github/sansarch/quick_cart/QuickCartApplication.java
@@ -2,8 +2,10 @@ package com.github.sansarch.quick_cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class QuickCartApplication {
 
 	public static void main(String[] args) {
